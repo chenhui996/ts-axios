4795869453857baf31e0052d24de7d808c09d5c1
cain <chen562450669@qq.com>
2021-11-26 19:31:36 +0800

    feat: add example -> error

---
+++ examples/error/app.ts
@@ -0,0 +1,54 @@
+import axios from '../../src/index'
+
+axios({
+  method: 'get',
+  url: '/error/get1'
+})
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  }) // 请求错误
+
+// ---------------------------------------------------------------------
+
+axios({
+  method: 'get',
+  url: '/error/get'
+})
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => {
+    console.log(e)
+  }) // 这里的e是axios的错误对象
+
+// ---------------------------------------------------------------------
+
+setTimeout(() => {
+  axios({
+    method: 'get',
+    url: '/error/get'
+  })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+}, 5000) // 这里的时间是5秒
+
+// ---------------------------------------------------------------------
+
+axios({
+  method: 'get',
+  url: '/error/timeout',
+  timeout: 2000
+})
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => {
+    console.log(e.message)
+  }) // 超时
