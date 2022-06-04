c59d7ddd87a2d993c136aebf258bc404dd9fa76c
cainchen996 <cainchen996@gmail.com>
2022-06-05 01:57:25 +0800

    ci(test/ts-axios.test.ts): annotation test/ts-axios.test.ts
    default test/ts-axios.test.ts -> error

--- test/ts-axios.test.ts
+++ test/ts-axios.test.ts
@@ -1,14 +1,13 @@
-import DummyClass from '../src/ts-axios'
+// import DummyClass from '../src/ts-axios'
 
 /**
  * Dummy test
  */
 describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
-
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
+  // it('works if true is truthy', () => {
+  //   expect(true).toBeTruthy()
+  // })
+  // it('DummyClass is instantiable', () => {
+  //   expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  // })
 })
