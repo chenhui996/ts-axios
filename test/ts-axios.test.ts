6fcfe48c3009438462af05d81b942764f1612b54
cainchen996 <cainchen996@gmail.com>
2022-06-05 02:01:41 +0800

    fix(test file): error -> Your test suite must contain at least one test.
    error -> Your test suite must contain at least one test.

--- test/ts-axios.test.ts
+++ test/ts-axios.test.ts
@@ -4,9 +4,9 @@
  * Dummy test
  */
 describe('Dummy test', () => {
-  // it('works if true is truthy', () => {
-  //   expect(true).toBeTruthy()
-  // })
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
   // it('DummyClass is instantiable', () => {
   //   expect(new DummyClass()).toBeInstanceOf(DummyClass)
   // })
